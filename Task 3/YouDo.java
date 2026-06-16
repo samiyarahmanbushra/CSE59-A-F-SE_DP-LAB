17d9b9516f58397fd9d6c0b511f236a9914c3872
samiyarahmanbushra <samiyarahman561@gmail.com>
2026-06-16 09:52:27 +0600

    Update YouDo.java

--- Task 3/YouDo.java
+++ Task 3/YouDo.java
@@ -1,1 +1,54 @@
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user = new User("john_doe", "john@example.com");
+
+        if (EmailValidator.isValidEmail(user.getEmail())) {
+            UserRepository repository = new UserRepository();
+            repository.save(user);
+        }
+    }
+}
+
+class User {
+    private String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
+class EmailValidator {
+
+    public static boolean isValidEmail(String email) {
+        if (email != null && email.contains("@")) {
+            return true;
+        }
+
+        System.out.println("Invalid email format.");
+        return false;
+    }
+}
+
+
+class UserRepository {
+
+    public void save(User user) {
+        System.out.println("Connecting to database...");
+        System.out.println(
+                "Saving user " + user.getUsername() + " to the users table."
+        );
+    }
+}
