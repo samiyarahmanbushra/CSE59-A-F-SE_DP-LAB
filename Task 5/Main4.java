709440e9e808c9011ba763f660d520c57892001f
samiyarahmanbushra <samiyarahman561@gmail.com>
2026-06-30 10:20:59 -0500

    Update Main4.java

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,36 @@
+interface OptimizableModel {
+    void quantize();
+}
 
+class ResNet18 implements OptimizableModel {
+    public void quantize() {
+        System.out.println("Applying int8 quantization for ResNet-18.");
+    }
+}
+
+class MobileNet implements OptimizableModel {
+
+
+    public void quantize() {
+        System.out.println("Applying dynamic quantization for MobileNet.");
+    }
+}
+
+class InferenceEngine {
+
+    public void optimizeModel(OptimizableModel model) {
+        model.quantize();
+    }
+}
+public class Main4 {
+    public static void main(String[] args) {
+
+        InferenceEngine engine = new InferenceEngine();
+
+        OptimizableModel resnet = new ResNet18();
+        engine.optimizeModel(resnet);
+
+        OptimizableModel mobileNet = new MobileNet();
+        engine.optimizeModel(mobileNet);
+    }
+}
