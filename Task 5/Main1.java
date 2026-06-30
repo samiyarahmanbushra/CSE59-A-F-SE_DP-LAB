5283f7a4051a3dbe743b30624528f7f01793cc6d
samiyarahmanbushra <samiyarahman561@gmail.com>
2026-06-30 10:03:54 -0500

    Update Main1.java

--- Task 5/Main1.java
+++ Task 5/Main1.java
@@ -1,1 +1,47 @@
 
+interface Printer {
+    void printDocument();
+}
+interface Scanner {
+    void scanDocument();
+}
+
+interface FaxMachine {
+    void faxDocument();
+}
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+
+    
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+
+
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+
+
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
+class BasicPrinter implements Printer {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
+public class Main1 {
+    public static void main(String[] args) {
+
+        BasicPrinter basicPrinter = new BasicPrinter();
+        basicPrinter.printDocument();
+
+        MultiFunctionCopier copier = new MultiFunctionCopier();
+        copier.printDocument();
+        copier.scanDocument();
+        copier.faxDocument();
+    }
+}
