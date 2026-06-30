34b84594f9a7387f3e47ed704b7e4323d185cdcc
samiyarahmanbushra <samiyarahman561@gmail.com>
2026-06-30 05:30:46 -0500

    Update YouDo8.java

--- Task 2/YouDo8.java
+++ Task 2/YouDo8.java
@@ -1,1 +1,36 @@
+class Item {
+    String name;
+    double price;
+
+    Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class Order {
+    Item item1;
+    Item item2;
+
+    Order(Item item1, Item item2) {
+        this.item1 = item1;
+        this.item2 = item2;
+    }
+
+    void printBill() {
+        double total = item1.price + item2.price;
+        System.out.println("Total Cost: " + total);
+    }
+}
+
+public class Main8 {
+    public static void main(String[] args) {
+        Item item1 = new Item("Mouse", 20.0);
+        Item item2 = new Item("Keyboard", 45.0);
+
+        Order order = new Order(item1, item2);
+
+        order.printBill();
+    }
+}
 
